3101566cb34567ce1f0a5a4211fd41035c7cb33c
Christoph Menzinger <Christoph.Menzinger@edu.campus02.at>
2021-03-13 19:26:30 +0100

    added new Drink Whisky with new attribute

---
+++ src/main/java/at/fhj/iit/Whiskey.java
@@ -0,0 +1,51 @@
+package at.fhj.iit;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+
+public class Whiskey extends Drink{
+
+    private int age = 15;
+    private String region;
+    private String bottling;
+
+    /**
+     * Creates a Drink object with given name, e.g. juice or orange juice
+     *
+     * @param name name of the drink
+     */
+    public Whiskey(String name) {
+        super(name);
+    }
+
+    @Override
+    public double getVolume() {
+        return 0;
+    }
+
+    @Override
+    public double getAlcoholPercent() {
+        return 39;
+    }
+
+    @Override
+    public boolean isAlcoholic() {
+        return false;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public String getRegion(){
+        return this.region;
+    }
+
+    public String getBottling(){
+        return this.bottling;
+    }
+}
+
+
+
