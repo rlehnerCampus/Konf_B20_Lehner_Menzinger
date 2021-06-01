046c8ac942e44ca10f8b3ef8ef170dd95856c9de
Robert Lehner <robert.lehner@edu.campus02.at>
2021-06-01 17:10:21 +0200

    Added class CashRegister.java
    Created new Branch for Abgabe03 and added class CashRegister.java

---
+++ src/main/java/at/fhj/iit/CashRegister.java
@@ -0,0 +1,12 @@
+/**
+*   <h1>Konf_B20_Lehner_Menzinger - CashRegister.java</h1>
+*   Cash register with report functionalities
+*   @author Robert LEHNER <a href="mailto:robert.lehner@edu.campus02.at">robert.lehner@edu.campus02.at</a>
+*   @version 2021-06-01
+*/
+
+package at.fhj.iit;
+public class CashRegister
+{
+
+}
